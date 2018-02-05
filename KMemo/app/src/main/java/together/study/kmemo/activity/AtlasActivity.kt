08f9b29f493629fed69aec4f144ee982b0dd041f
atlas <atlas9530@gmail.com>
2018-02-06 00:56:39 +0900

    [add] 스토리지 퍼미션 샘플 업로드

--- KMemo/app/src/main/java/together/study/kmemo/activity/AtlasActivity.kt
+++ KMemo/app/src/main/java/together/study/kmemo/activity/AtlasActivity.kt
@@ -1,15 +1,57 @@
 package together.study.kmemo.activity
 
+import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import together.study.kmemo.R
 
 class AtlasActivity : Activity() {
+
+    val REQ_CODE = 1001
+    val TAG = "Permission"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // hello creaby 
         setContentView(R.layout.activity_main_atlas)
+
+        setUpPermission()
+    }
+
+    private fun setUpPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                 reqeustPermission()
+        }
+    }
+
+    private fun reqeustPermission() {
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), REQ_CODE)
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            REQ_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
